1041ab8bb0650547011fd2c4ebe54f55ce19958d
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-01-27 08:59:10 +0100

    correction sur la combinaison de 2 nombres

--- eau01-js.js
+++ eau01-js.js
@@ -1,22 +1,27 @@
 const twoElements = () => {
-
     let results = '';
 
-    // utilisation de ASCII
+    // Utilisation de ASCII
     for (let code = 48; code <= 57; code++) {
         for (let code2 = 48; code2 <= 57; code2++) {
-            results += String.fromCharCode(code, code2) + ',';
+            for (let code3 = 48; code3 <= 57; code3++) {
+                for (let code4 = 48; code4 <= 57; code4++) {
+                    // Vérifier si ce n'est pas "00 00" et "99 99"
+                    if (!(code === 48 && code2 === 48 && code3 === 48 && code4 === 48) &&
+                        !(code === 57 && code2 === 57 && code3 === 57 && code4 === 57)) {
+                        results += String.fromCharCode(code) + String.fromCharCode(code2) + ' ' + String.fromCharCode(code3) + String.fromCharCode(code4) + ',';
+                    }
+                }
+            }
         }
     } 
 
-return results.slice(0, -1);
-
+    return results.slice(0, -1);
 }
 
 const errorWithoutArguments = () => {
-
     if (process.argv[2]) {
-        console.log("erreur : l'argument n'est pas nécessaire dans le script. ");
+        console.log("Erreur : L'argument n'est pas nécessaire dans le script.");
     } else {
         console.log(twoElements());
     }
