0adbb931e82216ffb37b2e6483441b1646c85b68
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-01-28 15:20:00 +0100

    prochain nombre entier

---
+++ eau04-js.js
@@ -0,0 +1,45 @@
+const userString = () => {
+    return parseInt(process.argv[2]);
+}
+
+const isPrimeNumber = (number) => {
+    if (number <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+const nextPrimeNumber = (userNumber) => { 
+    while (!isPrimeNumber(userNumber + 1)) {
+        userNumber++;
+    }
+
+    return userNumber + 1;
+}
+
+const errorNotNumber = (userString) => {
+    if (isNaN(userString)) {
+        throw new Error ("Erreur : veuillez choisir un nombre entier.");
+    }
+
+    if (userString <= 1) {
+        throw new Error ("Erreur : veuillez chosir un nombre positif et qui ne soit pas égal à 0 ou 1")
+    }
+
+}
+
+try {
+    const userInput = userString();
+    errorNotNumber(userInput);
+    const result = nextPrimeNumber(userInput);
+    console.log(result);
+} catch (error) {
+    console.error(error.message);
+}
