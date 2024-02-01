5941befd543d4250b4b1b44138a9f9594f0eff39
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-02-01 16:11:12 +0100

    index wanted

--- eau10-js.js
+++ eau10-js.js
@@ -1,30 +1,27 @@
 const userStrings = () => {
-    return [parseInt(process.argv[2]), parseInt(process.argv[3])];
-}
+    return process.argv.slice(2);
+};
 
-const minMax = () => {
-    const strings = userStrings();
-    const userString1 = strings[0];
-    const userString2 = strings[1];
-
-    let result = '';
-    for (let i = userString1; i < userString2; i++) {
-        result += i + ' ';
+const indexWanted = (userInput) => {
+    const lastArgument = userInput[userInput.length - 1];
+    let position = -1;
+    for (let i = 0; i < userInput.length - 1; i++) {
+        if (userInput[i] === lastArgument) {
+            position = i;
+            break;
+        }
     }
-    return result;
-}
 
-const handlingError = (strings) => {
-    if (!strings || isNaN(strings[0]) || isNaN(strings[1])) {
-        throw new Error("Erreur : veuillez écrire deux nombres valides.");
+    if (position === -1) {
+        throw new Error("-1");
     }
-    return strings;
-}
+
+    return position;
+};
 
 try {
-    const strings = userStrings();
-    const validatedStrings = handlingError(strings);
-    const result = minMax(validatedStrings);
+    const userInput = userStrings();
+    const result = indexWanted(userInput);
 
     console.log(result);
 } catch (error) {
