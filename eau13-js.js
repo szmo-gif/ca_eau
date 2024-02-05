cdee029f0a34b9b4f20be497695d16e3d0519e96
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-02-05 12:11:51 +0100

    tri par selection

---
+++ eau13-js.js
@@ -0,0 +1,35 @@
+const userString = () => {
+    return process.argv.slice(2);
+} 
+
+selectionSort = (userInput) => {
+
+    for (let i = 0; i < userInput.length - 1; i++) {
+    let minIndex = i;
+
+        for (let j = i + 1; j < userInput.length; j++) {
+            if (parseInt(userInput[j]) < parseInt(userInput[minIndex])) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex !== i) {
+            let temp = userInput[i];
+            userInput[i] = userInput[minIndex];
+            userInput[minIndex] = temp;
+        }
+    }
+
+    return userInput.join(' ')
+}
+
+const handlingError =(userInput) => {
+    if (!userInput.length || userInput.some(isNaN)) {
+        console.log("Error : écrivez une suite de nombres");
+        process.exit(1);
+    }
+}
+  
+const userInput = userString();
+handlingError(userInput);
+console.log(selectionSort(userInput));
