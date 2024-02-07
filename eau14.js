e964b7b49ec6b6889e18399e718a3d99b4de620e
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-02-07 14:14:49 +0100

    ascii + reste du mots

--- eau14.js
+++ eau14.js
@@ -1,45 +1,48 @@
 const wordsArray = () => {
     return process.argv.slice(2);
-} 
+};
 
 const translateWordInAscii = (wordsArray) => {
     let array = [];
     for (let i = 0; i < wordsArray.length; i++) {
-        const asciivalue = wordsArray[i].charCodeAt(0);
-        array.push(asciivalue);
+        let asciiValue = wordsArray[i].charCodeAt(0) + wordsArray[i].slice(1);
+        array.push(asciiValue);
     }
-    return array
-}
+    return array;
+};
 
-const translateAsciiInWord = (AsciiArray) => {
+const translateAsciiInWord = (asciiArray) => {
     let array = [];
-    for (let i = 0; i < AsciiArray.length; i++) {
-        const wordValue = String.fromCharCode(AsciiArray[i]);
+    for (let i = 0; i < asciiArray.length; i++) {
+        let wordValue = String.fromCharCode(asciiArray[i]) + asciiArray[i].slice(1);
         array.push(wordValue);
     }
-    return array
-}
+    return array;
+};
 
 const selectionSort = (array) => {
-
     for (let i = 0; i < array.length; i++) {
-    let minIndex = i;
-
+        let minIndex = i;
         for (let j = i + 1; j < array.length; j++) {
             if (parseInt(array[j]) < parseInt(array[minIndex])) {
                 minIndex = j;
             }
         }
-
         if (minIndex !== i) {
             let temp = array[i];
             array[i] = array[minIndex];
             array[minIndex] = temp;
         }
     }
+    return array;
+};
 
-    return console.log(array.join(' '))
-}
-
-
-translateAsciiInWord(selectionSort(translateAsciiInWord(wordsArray())));
+const words = wordsArray();
+const asciiValues = translateWordInAscii(words);
+const sortedAsciiValues = selectionSort(asciiValues);
+const sortedWords = translateAsciiInWord(asciiValues);
+console.log(words);
+console.log(asciiValues);
+console.log(sortedWords);
+//console.log(sortedAsciiValues);
+//console.log(sortedWords.join(' '));
