21dc12e0c71365bdf9bb31c301c11e5a01f03e8d
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-02-09 10:20:19 +0100

    tri par ordre ascii

--- eau14.js
+++ eau14.js
@@ -3,25 +3,65 @@ const wordsArray = () => {
     let array = [];
 
     user.forEach(element => {
-        let words = element.split(' ');
+        let words = element.split('');
         array.push(words);
     });
     return array;
 };
 
-const asciiComparison = (a, b) => {
-    for (let i = 0; i < Math.min(a.length, b.length); i++) {
-        if (a.charCodeAt(i) !== b.charCodeAt(i)) {
-            return a.charCodeAt(i) - b.charCodeAt(i);
-        }
-    }
-    return a.length - b.length;
+const translateLetterToAscii = (letter) => {
+    return letter.charCodeAt(0);
 };
 
-const user = wordsArray();
-user.forEach(array => {
-    array.sort(asciiComparison);
-    console.log(array); // Afficher les mots triés pour chaque tableau interne
-});
+const translateAsciiToLetter = (asciiValue) => {
+    return String.fromCharCode(asciiValue);
+};
 
-console.log(user); // Afficher les tableaux de mots triés
+const translateWordToAscii = (word) => {
+    let asciiArray = [];
+
+    word.forEach(letter => {
+        let asciiValue = translateLetterToAscii(letter);
+        asciiArray.push(asciiValue);
+    });
+
+    return asciiArray;
+};
+
+const translateAsciiToWord = (asciiArray) => {
+    let word = '';
+
+    asciiArray.forEach(asciiValue => {
+        let letter = translateAsciiToLetter(asciiValue);
+        word += letter;
+    });
+
+    return word;
+};
+
+const sortWordsByAscii = (wordsArray) => {
+    let asciiWords = [];
+
+    wordsArray.forEach(word => {
+        let asciiWord = translateWordToAscii(word);
+        asciiWords.push(asciiWord);
+    });
+
+    asciiWords.sort();
+
+    return asciiWords;
+};
+
+const handleError = () => {
+    if (!process.argv.slice(2)) {
+        console.log("Ecrivez une ou des chaines de caractères");
+        process.exit(1)
+    }
+}
+
+const user = wordsArray();
+handleError(user)
+const sortedAsciiWords = sortWordsByAscii(user);
+const sortedWords = sortedAsciiWords.map(word => translateAsciiToWord(word));
+
+console.log(sortedWords.join(' '));
