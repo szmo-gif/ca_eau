0cec6c7f909e29efdd8fda7422ccc5a0e2ee5f85
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-02-13 18:03:29 +0100

    norme js

--- eau05.js
+++ eau05.js
@@ -1,14 +1,17 @@
-const userArray = () => {
-    return [process.argv[2], process.argv[3]];
+const getArgument = () => {
+    return process.argv.slice(2);
 }
 
-const stringInString = () => {
-    let strings = userArray();
-    let string1 = strings[0];
-    let string2 = strings[1];
-
+const stringInString = (string1, string2) => {
     for (let i = 0; i <= string1.length - string2.length; i++) {
-        if (string1.slice(i, i + string2.length) === string2) {
+        let match = true;
+        for (let j = 0; j < string2.length; j++) {
+            if (string1[i + j] !== string2[j]) {
+                match = false;
+                break;
+            }
+        }
+        if (match) {
             return true;
         }
     }
@@ -16,12 +19,21 @@ const stringInString = () => {
     return false;
 }
 
-const handleError = (strings) => {
-    if (!strings || strings.some(str => typeof str !== 'string')) {
+const validTwoStrings = (strings) => {
+    if (strings.length === 2 && strings.every(str => typeof str === 'string')) {
+        return strings;
+    } else {
         console.log("Veuillez entrer deux chaînes de caractères.");
-        process.exit(1)
+        process.exit(1);
     }
 }
 
-handleError(userArray());
-console.log(stringInString(userArray()));
+const applyFunction = () => {
+    const userArguments = getArgument();
+    const validArguments = validTwoStrings(userArguments);
+    const includes = stringInString(validArguments[0], validArguments[1]);
+
+    console.log(includes);
+}
+
+applyFunction();
